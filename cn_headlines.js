5358f8e7ff4e367f76e9765dd48805b7325e67e6
jianshentan <jianshentan@gmail.com>
2014-07-22 20:46:37 -0400

    finsihed

--- cn_headlines.js
+++ cn_headlines.js
@@ -2,8 +2,9 @@ var Twit = require( 'twit' ),
     request = require( 'request' ),
     fs = require( 'fs' ),
     nodehun = require( 'nodehun' ),
-    client = require( 'node-wolfram' )
-    async = require( 'async' );
+    client = require( 'node-wolfram' ),
+    async = require( 'async' ),
+    BitlyAPI = require( 'node-bitlyapi' );
 
 // Twitter data
 var T = new Twit({
@@ -15,18 +16,29 @@ var T = new Twit({
 
 // USA Today
 var api_key = "8vcg49qy5hvcp5468ht8se6k";
-var articles_key = "b9psy38uexpyna4ku6h4dqyw";
-var breaking_news_key = "jejf4v2sw882esr2xxjx67fk";
+var articles_key = "b9psy38uexpyna4ku6h4dqyw"; // not in use
+var breaking_news_key = "jejf4v2sw882esr2xxjx67fk"; // not in use
 
 // Wolfram
 var wolfram = new client( 'JUWWE6-E4AV426PQU' );
+
+// Bitly
+var Bitly = new BitlyAPI({
+    client_id: "bea395ef75eb58141728560b0ae1976a74c76090",
+    client_secret: "76ac89457a04df829a9c9d1add5412bd0faf4583"
+});
+Bitly.setAccessToken( "a66826599cbedaba3ccb1ff36f4306f5e577b7f6" );
+
+// Log files
+var TWEET_LOG = "tweet_log.txt";
+
 
 // ============================================================== //
 // refreshes array of stories everyday
 
 var stories = [];
 
-//getArticles();
+getArticles();
 setInterval( getArticles, 86400000 );
 function getArticles() {
     var url = "http://api.usatoday.com/open/articles/topnews?api_key=" + 
@@ -35,7 +47,6 @@ function getArticles() {
         if( err ) { console.log( err ); }
         else {
             stories = JSON.parse( body ).stories;
-            console.log( stories );
         } 
     });
 };
@@ -94,11 +105,10 @@ function createHeadline( headline, cb ) {
             }
         },
         function( err ) {
-            console.log( "NEW WORDS ARRAY" );
-            console.log( newWords );
-
             var sentence = processSentence( newWords );
 
+            console.log( headline ); // -- original
+            console.log( sentence ); // -- new
             if( !hasName ) {
                 return cb( null );
             } else {
@@ -116,13 +126,13 @@ function processSentence( words ) {
             if( i<words.length-1 ) {
                 var nextWord = words[i+1];
                 if( nextWord === "NAME" ) {
-                    sentence += "Chinese Official, " + randomChineseName() + " ";
+                    sentence += randomChinesePosition() + ", " + randomChineseName() + " ";
                     i++;
                 } else {
-                    sentence += "Chiense Official, " + randomChineseName() + " ";
+                    sentence += randomChinesePosition() + ", " + randomChineseName() + " ";
                 }
             } else {
-                sentence += "Chiense Official, " + randomChineseName() + " ";
+                sentence += randomChinesePosition() + ", " + randomChineseName() + " ";
             }
         } else if( word === "PLACE" ) {
             sentence += "China "; 
@@ -130,12 +140,17 @@ function processSentence( words ) {
             sentence += word + " ";
         }
     }
-    console.log( sentence );
     return sentence;
 };
 
+function randomChinesePosition() {
+    var positionArray = [ "Chinese Military Officer", "Chinese Communist Party Leader", "Chinese Official", "Chinese Government Executive", "Chinese Party Officer" ];
+    return positionArray[ Math.floor( Math.random()*positionArray.length ) ];
+};
+
 function randomChineseName() {
-    return "Hua Liuwei";
+    var nameArray = [ "Hua Liuwei", "Dong Jianjing", "Chen Ruo", "Wang Feng", "Xi Jianping", "Zhou Huarei", "Ma Zhen", "Xiu Chongan", "Che Xueqin", "Kam Xiang", "Mu Hulin", "Lei Kueng", "Cong Guon", "Ww Shangyuan", "Ao Yingjie", "Liew Zhen", "Yip Jiang", "Hsueh Sueh-yen", "Wu Shan" ];
+    return nameArray[ Math.floor( Math.random()*nameArray.length ) ];
 };
 
 function wordType( word, cb ) {
@@ -164,7 +179,6 @@ function wordType( word, cb ) {
                 }
             });
         }], function( err, results ) {
-            console.log( results );
             if( err )
                 console.log( err );
             else {
@@ -230,19 +244,79 @@ function isLetter( c ) {
 
 
 // ============================================================== //
-// runs main program
+// runs main program // builds and sends tweet
+
+
+run();
+setInterval( run, 7200000 );
 
 function run() {
-    if( stories.length === 0 ) {
+    if( stories.length == 0 ) {
         getArticles();
+        setTimeout( buildTweet, 20000 );
+        return;
     }
-    // createHeadline( sentence, cb )
-    var originalTitle = stories[0].title;
-    var link = stories[0].link;
-    var pubDate = stories[0].pubDate;
-    function onNewHeadline( res ) {
-        
+    buildTweet();
+};
+
+function buildTweet() {
+    var originalHeadline = stories[1].title;
+    var link = stories[1].link;
+
+    async.series([
+        function( callback ) {
+            // bitlyfy url
+            Bitly.shortenLink( link, function( err, res ) {
+                if( err ) {
+                    callback( "Cannot bitlyfy", null );
+                } else {
+                    res = JSON.parse( res );
+                    if( res.status_code !== 200 ) {
+                        callback( "Cannot bitlyfy", null );
+                    } else {
+                        callback( null, res.data.url );
+                    }
+                }
+            });
+        },
+        function( callback ) {
+            // create new headline
+            createHeadline( originalHeadline, function( res ) {
+                if( !res ) {
+                    callback( "Headline creation failed or no Names were found", null );
+                } else {
+                    callback( null, res ); 
+                }
+                // remove first story
+                stories.shift();
+            });
+        }],
+        function( err, results ) {
+            // create tweet
+            if( err ) {
+                console.log( err );
+            } else {
+                var tweet = results[1] + results[0];
+                T.post( 'statuses/update', { status: tweet }, function( err, data, response ) {
+                    if( err ) {
+                        console.log( err );
+                    } else {
+                        logTweet( data[ "text" ] );
+                        console.log( tweet );
+                    }
+                });
+            }
+        });
+};
+
+// ============================================== //
+// 
+
+function logTweet( tweet ) {
+    if( !fs.existsSync( TWEET_LOG ) ) {
+        fs.openSync( TWEET_LOG, 'w' );
     }
+    fs.writeFileSync( TWEET_LOG, tweet );
 };
 
 
